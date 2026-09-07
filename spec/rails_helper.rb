be2c317e3161b41b66c4c22e503246e13f5bb447
nitin_tandukar <nitin.tandukar@bajratechnologies.com>
2026-09-07 10:23:27 +0545

    style: fix rubocop offenses in scaffold files
    Refs T1

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,6 +72,6 @@ RSpec.configure do |config|
 
   # Drive system specs with Selenium against headless Chrome.
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
   end
 end
